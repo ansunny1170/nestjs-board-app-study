267431e0139f38a540fcc4e2623edb66c7102763
yechankim <ansunny1170@gmail.com>
2023-02-10 13:30:33 +0900

    feat: user validationPipe 기능 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialDto {
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(20)
     @ApiProperty({ description: 'username', example: 'kyc' })
     username: string;
 
     @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: 'password only accepts english and number'
+    })
     @ApiProperty({ description: 'password', example: '' })
     password: string;
 }
