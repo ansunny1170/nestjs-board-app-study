83ef627ffdb0ec177e3963abe61788e02fcdd0b6
yechankim <ansunny1170@gmail.com>
2023-02-10 16:39:09 +0900

    feat: /auth/test에서 커스텀 @를 만들어 user정보만 받는 기능 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { User } from './auth.entity';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -18,12 +20,17 @@ export class AuthController {
         return this.authService.signIn(authCredentialDto)
     }
 
-    @UseGuards(AuthGuard('jwt')) 
+    @UseGuards(AuthGuard()) 
     @Post('/test')
     // UseGuards안에 @nestjs/passport에서 가져온 AuthGuard()를 이용하면 요청안에 유저 정보를 넣어줄 수있다.
     // 그런데 swagger에서 accessToken 사용방법을 모르겠다. `Authorize`에 입력해도 적용이 안된다.
     // postman으로 하면 정상 작동함
     test(@Req() req) {
         console.log('req', req)
+    }
+    @UseGuards(AuthGuard()) 
+    @Post('/test1')
+    test1(@GetUser() user: User) {
+        console.log('req', user)
     }
 }
