340e51e91a4a6f040968b83a50f1c47b0a40ecea
yechankim <ansunny1170@gmail.com>
2023-02-10 14:11:56 +0900

    feat: user의 password 암호화 기능 추가

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { DataSource, Repository } from "typeorm";
 import { User } from "./auth.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
+import * as bcrypt from 'bcryptjs'
 
 export class UserRepository extends Repository<User> {
     constructor(@InjectRepository(User) private dataSource: DataSource) {
@@ -11,7 +12,10 @@ export class UserRepository extends Repository<User> {
 
     async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
         const { username, password } = authCredentialDto;
-        const user = this.create({ username, password })
+        
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const user = this.create({ username, password: hashedPassword })
 
         try {
             await this.save(user);        
